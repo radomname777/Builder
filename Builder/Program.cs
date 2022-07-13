1025d649e4f290fe20eefffcf7dc67931d21a993
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2022-07-13 15:35:50 +0400

    0.1

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -2,23 +2,19 @@
 
 namespace Builder;
 
-
-// Product
-// IBuilder
-// ConcreteBuilder1, ConcreteBuilder2
-// Director (Optional)
-
-
-
 class Car
 {
+    public string? Name { get; set; }
     public int Seats { get; set; }
     public double Engine { get; set; }
     public bool TripComputer { get; set; }
     public bool GPS { get; set; }
-
+    public Car(string? name)
+    {
+        Name = name;
+    }
     public override string ToString()
-    => $"Seats = {Seats}\nEngine{Engine}\nTripComputer{TripComputer}\nGPS{GPS}";
+    => $"Name = {Name}\nSeats = {Seats}\nEngine{Engine}\nTripComputer{TripComputer}\nGPS{GPS}";
 }
 
 
@@ -33,9 +29,9 @@ interface Bulider
     Car Result();
 }
 
-class WoodHouseBuilder : Bulider
+class CarBuilder : Bulider
 {
-    public Car car { get; set; } = new Car();
+    public Car car { get; set; } = new Car("CarBulider");
 
     public Bulider Engine(double engine)
     {
@@ -47,7 +43,7 @@ class WoodHouseBuilder : Bulider
         car.GPS = true; return this;
     }
 
-    public void Reset() => car = new Car();
+    public void Reset() => car = new Car(car.Name);
     public Car Result() => car;
 
     public Bulider Seats(int number)
@@ -59,98 +55,44 @@ class WoodHouseBuilder : Bulider
     {
         car.TripComputer = true; return this;
     }
-    //public House House { get; set; } = new House { Name = "WoodHouse" };
-
-    //public IHouseBuilder BuildDoors(int count)
-    //{
-    //    House.Doors = count;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildGarage()
-    //{
-    //    House.HasGarage = true;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildGarden()
-    //{
-    //    House.HasGarden = true;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildPool()
-    //{
-    //    House.HasPool = true;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildRoof()
-    //{
-    //    House.HasRoof = true;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildWalls(int count)
-    //{
-    //    House.Walls = count;
-    //    return this;
-    //}
-    //public IHouseBuilder BuildWindows(int count)
-    //{
-    //    House.Windows = count;
-    //    return this;
-    //}
-    //public House Build() => House;
-
-    //public void Reset() => House = new House();
 
 }
 
-//class StoneHouseBuilder : IHouseBuilder
-//{
-//    public House House { get; set; } = new House { Name = "StoneHouse" };
+class CarManualBuilder : Bulider
+{
+    public Car car { get; set; } = new Car("CarManualBuilder");
 
-//    public IHouseBuilder BuildDoors(int count)
-//    {
-//        House.Doors = count;
-//        return this;
-//    }
-//    public IHouseBuilder BuildGarage()
-//    {
-//        House.HasGarage = true;
-//        return this;
-//    }
-//    public IHouseBuilder BuildGarden()
-//    {
-//        House.HasGarden = true;
-//        return this;
-//    }
-//    public IHouseBuilder BuildPool()
-//    {
-//        House.HasPool = true;
-//        return this;
-//    }
-//    public IHouseBuilder BuildRoof()
-//    {
-//        House.HasRoof = true;
-//        return this;
-//    }
-//    public IHouseBuilder BuildWalls(int count)
-//    {
-//        House.Walls = count;
-//        return this;
-//    }
-//    public IHouseBuilder BuildWindows(int count)
-//    {
-//        House.Windows = count;
-//        return this;
-//    }
-//    public House Build() => House;
+    public Bulider Engine(double engine)
+    {
+        car.Engine = engine; return this;
+    }
 
-//    public void Reset() => House = new House();
-//}
+    public Bulider GPS()
+    {
+        car.GPS = true; return this;
+    }
+
+    public void Reset() => car = new Car(car.Name);
+    public Car Result() => car;
+
+    public Bulider Seats(int number)
+    {
+        car.Seats = number; return this;
+    }
+
+    public Bulider TripComputer()
+    {
+        car.TripComputer = true; return this;
+    }
+
+}
+
 class Program
 {
     static void Main()
     {
 
-        Bulider builder = new WoodHouseBuilder();
+        Bulider builder = new CarBuilder();
 
 
         Car house = builder
